502de94c5ed7c58d839f50b3bce400fc43062ead
William Tait <wiltait@gmail.com>
2023-03-13 08:03:08 +0000

    Add FounderProfile component for fetching data and displaying profile

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
+import axios from 'axios';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.config.globalProperties.$http = axios;
+
+app.mount('#app');
